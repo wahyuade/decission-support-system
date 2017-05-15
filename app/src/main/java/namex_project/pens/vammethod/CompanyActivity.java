d58cadfe5db7e75b8a5d88cc9415bae756da1d3b
Wahyu Ade Sasongko <wahyuadesasongko@gmail.com>
2017-05-15 17:37:11 +0700

    Fresh Project

---
+++ app/src/main/java/namex_project/pens/vammethod/CompanyActivity.java
@@ -0,0 +1,13 @@
+package namex_project.pens.vammethod;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CompanyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_company);
+    }
+}
