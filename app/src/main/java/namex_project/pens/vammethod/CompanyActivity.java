29bf66083ef35cffff8ea8135cdfb17e96561cc9
Wahyu Ade Sasongko <wahyuadesasongko@gmail.com>
2017-05-15 18:45:06 +0700

    just try

--- app/src/main/java/namex_project/pens/vammethod/CompanyActivity.java
+++ app/src/main/java/namex_project/pens/vammethod/CompanyActivity.java
@@ -2,12 +2,18 @@ package namex_project.pens.vammethod;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.GridView;
+
+import java.util.ArrayList;
 
 public class CompanyActivity extends Activity {
-
+    GridView list_company;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company);
+
+        list_company = (GridView)findViewById(R.id.list_company);
+
     }
 }
