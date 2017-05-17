3827515940e452b70f1d11f27e3c970038cf5fc6
Wahyu Ade Sasongko <wahyuadesasongko@gmail.com>
2017-05-17 15:07:31 +0700

    pup up menu ketika klik company

--- app/src/main/java/namex_project/pens/vammethod/Activity/Company/CompanyGridAdapter.java
+++ app/src/main/java/namex_project/pens/vammethod/Activity/Company/CompanyGridAdapter.java
@@ -1,7 +1,9 @@
 package namex_project.pens.vammethod.Activity.Company;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.ViewHolder;
@@ -16,6 +18,7 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 
+import namex_project.pens.vammethod.Activity.EditData.EditDataActivity;
 import namex_project.pens.vammethod.Database.Model.CompanyModel;
 import namex_project.pens.vammethod.R;
 
@@ -28,6 +31,8 @@ public class CompanyGridAdapter extends RecyclerView.Adapter<CompanyGridAdapter.
     private ArrayList<CompanyModel> data_company;
     private CompanyActivity companyActivity;
 
+    TextView company_name;
+    LinearLayout edit_data, route;
     public CompanyGridAdapter(Activity activity, ArrayList<CompanyModel> data_company, CompanyActivity companyActivity) {
         this.activity = activity;
         this.data_company = data_company;
@@ -58,11 +63,37 @@ public class CompanyGridAdapter extends RecyclerView.Adapter<CompanyGridAdapter.
     }
 
     @Override
-    public void onBindViewHolder(CompanyGrid holder, int position) {
+    public void onBindViewHolder(CompanyGrid holder, final int position) {
         holder.company_item.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(activity, "Ini perusahaan gan", Toast.LENGTH_SHORT).show();
+                LayoutInflater inflater = activity.getLayoutInflater();
+                final AlertDialog menu_company = new AlertDialog.Builder(activity).create();
+                View interface_menu = inflater.inflate(R.layout.popup_menu_company, null);
+                menu_company.setView(interface_menu);
+
+                company_name = (TextView)interface_menu.findViewById(R.id.company_name);
+                edit_data = (LinearLayout)interface_menu.findViewById(R.id.edit_data);
+                route = (LinearLayout)interface_menu.findViewById(R.id.route);
+
+                company_name.setText(data_company.get(position).getName());
+                edit_data.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent edit_data_activity = new Intent(activity, EditDataActivity.class);
+                        edit_data_activity.putExtra("id", Integer.toString(data_company.get(position).getId()));
+                        menu_company.dismiss();
+                        activity.startActivity(edit_data_activity);
+                    }
+                });
+                route.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(activity, "Edit data is invalid !", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+                menu_company.show();
             }
         });
         holder.company_photo.setImageBitmap(BitmapFactory.decodeByteArray(data_company.get(position).getPhoto(),0,data_company.get(position).getPhoto().length));
