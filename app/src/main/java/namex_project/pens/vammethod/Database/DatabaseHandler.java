b00b91e9b859f2d8bf52a633ce96903700a58cb3
sherlyfekay <sherlyfekay@gmail.com>
2017-05-18 02:42:42 +0700

    tambahan database handler

--- app/src/main/java/namex_project/pens/vammethod/Database/DatabaseHandler.java
+++ app/src/main/java/namex_project/pens/vammethod/Database/DatabaseHandler.java
@@ -2,6 +2,7 @@ package namex_project.pens.vammethod.Database;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -9,6 +10,9 @@ import android.database.sqlite.SQLiteOpenHelper;
 import java.util.ArrayList;
 
 import namex_project.pens.vammethod.Database.Model.CompanyModel;
+import namex_project.pens.vammethod.Database.Model.CostModel;
+import namex_project.pens.vammethod.Database.Model.DestinationModel;
+import namex_project.pens.vammethod.Database.Model.SourceModel;
 
 /**
  * Created by SHERLY on 15/05/2017.
@@ -108,10 +112,11 @@ public class DatabaseHandler extends SQLiteOpenHelper {
 
     //Method insert tabel company
     public String insertCompany(CompanyModel data){
-        SQLiteDatabase db = this.getReadableDatabase();
+        SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
         contentValues.put(COMPANY_NAME, data.getName());
         contentValues.put(COMPANY_PHOTO, data.getPhoto());
+
         db.insert(TABLE_NAME_COMPANIES,null,contentValues);
         Cursor cur = db.query(TABLE_NAME_COMPANIES, new String[] {COMPANY_ID},COMPANY_NAME+"='"+data.getName()+"'",null,null,null,null);
         cur.moveToPosition(0);
@@ -120,6 +125,24 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         return id;
     }
 
+    //Method membaca satu company
+    public CompanyModel readCompany(int ID) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_NAME_COMPANIES, new String[] {
+                COMPANY_ID,
+                COMPANY_PHOTO,
+                COMPANY_NAME},
+                COMPANY_ID + "=?", new String[] {Integer.toString(ID)}, null, null, null);
+
+        if(cursor != null)
+            cursor.moveToFirst();
+
+        CompanyModel  data = new CompanyModel(Integer.parseInt(cursor.getString(0)), cursor.getBlob(1), cursor.getString(2));
+        return data;
+    }
+
+    //Membaca semua data company
     public ArrayList<CompanyModel> readCompanyAll(){
         SQLiteDatabase db = this.getReadableDatabase();
         ArrayList<CompanyModel> data = new ArrayList<CompanyModel>();
@@ -131,50 +154,149 @@ public class DatabaseHandler extends SQLiteOpenHelper {
         db.close();
         return data;
     }
-    //Method insert tabel source
-    public String insertSource(CompanyModel data){
-        String INSERT_COMPANY = "INSERT INTO "+TABLE_NAME_COMPANIES+" ("+
-                COMPANY_NAME + ")" + " VALUES ('"+
-                data.getName()+"'"+
-                ");";
 
-        SQLiteDatabase db = this.getReadableDatabase();
-        db.execSQL(INSERT_COMPANY);
-        Cursor cur = db.query(TABLE_NAME_COMPANIES, new String[] {COMPANY_ID},COMPANY_NAME+"="+data.getName(),null,null,null,null);
+    //Method insert tabel source
+    public String insertSource(SourceModel data){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(SOURCE_NAME, data.getName());
+        contentValues.put(SOURCE_CAPACITY, data.getCapacity());
+        contentValues.put(SOURCE_ID_COMPANY, data.getId_company());
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.insert(TABLE_NAME_SOURCES, null, contentValues);
+        Cursor cur = db.query(TABLE_NAME_SOURCES, new String[] {SOURCE_ID},SOURCE_NAME+"="+data.getName(),null,null,null,null);
         cur.moveToPosition(0);
         String id  = cur.getString(0);
         db.close();
         return id;
+    }
+
+    //Membaca satu source
+    public SourceModel readSource(int ID) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_NAME_SOURCES, new String[] {
+                        SOURCE_ID,
+                        SOURCE_ID_COMPANY,
+                        SOURCE_NAME,
+                        SOURCE_CAPACITY},
+                SOURCE_ID + "=?", new String[] {Integer.toString(ID)}, null, null, null);
+
+        if(cursor != null)
+            cursor.moveToFirst();
+
+        SourceModel  data = new SourceModel(Integer.parseInt(cursor.getString(0)), Integer.parseInt(cursor.getString(1)), Integer.parseInt(cursor.getString(2)), cursor.getString(3));
+        return data;
+    }
+
+    //Membaca semua data sources
+    public ArrayList<SourceModel> readSourcesAll(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        ArrayList<SourceModel> data = new ArrayList<SourceModel>();
+
+        Cursor cur = db.query(TABLE_NAME_SOURCES,new String[]{SOURCE_ID,SOURCE_ID_COMPANY,SOURCE_CAPACITY,SOURCE_NAME},null,null,null,null,null);
+        for (int cc=0; cc<cur.getCount();cc++){
+            cur.moveToPosition(cc);
+            data.add(new SourceModel(Integer.parseInt(cur.getString(0)),Integer.parseInt(cur.getString(1)),Integer.parseInt(cur.getString(2)),cur.getString(3)));
+        }
+        db.close();
+        return data;
     }
 
     //Method insert tabel destination
-    public String insertDestination(CompanyModel data){
-        String INSERT_COMPANY = "INSERT INTO "+TABLE_NAME_COMPANIES+" ("+
-                COMPANY_NAME + ")" + " VALUES ('"+
-                data.getName()+"'"+
-                ");";
+    public String insertDestination(DestinationModel data){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(DEST_NAME, data.getName());
+        contentValues.put(DEST_CAPACITY, data.getNeeded_num());
+        contentValues.put(DEST_ID_COMPANY, data.getId_company());
 
-        SQLiteDatabase db = this.getReadableDatabase();
-        db.execSQL(INSERT_COMPANY);
-        Cursor cur = db.query(TABLE_NAME_COMPANIES, new String[] {COMPANY_ID},COMPANY_NAME+"="+data.getName(),null,null,null,null);
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.insert(TABLE_NAME_DESTINATIONS, null, contentValues);
+        Cursor cur = db.query(TABLE_NAME_DESTINATIONS, new String[] {DEST_ID},DEST_NAME+"="+data.getName(),null,null,null,null);
         cur.moveToPosition(0);
         String id  = cur.getString(0);
         db.close();
         return id;
     }
 
-    public String insertCost(CompanyModel data){
-        String INSERT_COMPANY = "INSERT INTO "+TABLE_NAME_COMPANIES+" ("+
-                COMPANY_NAME + ")" + " VALUES ('"+
-                data.getName()+"'"+
-                ");";
-
+    //Method membaca satu destination
+    public DestinationModel readDestination(int ID) {
         SQLiteDatabase db = this.getReadableDatabase();
-        db.execSQL(INSERT_COMPANY);
-        Cursor cur = db.query(TABLE_NAME_COMPANIES, new String[] {COMPANY_ID},COMPANY_NAME+"="+data.getName(),null,null,null,null);
-        cur.moveToPosition(0);
-        String id  = cur.getString(0);
+
+        Cursor cursor = db.query(TABLE_NAME_DESTINATIONS, new String[] {
+                        DEST_ID,
+                        DEST_ID_COMPANY,
+                        DEST_CAPACITY,
+                        DEST_NAME},
+                DEST_ID + "=?", new String[] {Integer.toString(ID)}, null, null, null);
+
+        if(cursor != null)
+            cursor.moveToFirst();
+
+        DestinationModel  data = new DestinationModel(Integer.parseInt(cursor.getString(0)), Integer.parseInt(cursor.getString(1)), Integer.parseInt(cursor.getString(2)), cursor.getString(3));
+        return data;
+    }
+
+    //Membaca semua data destinations
+    public ArrayList<DestinationModel> readDestinationsAll(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        ArrayList<DestinationModel> data = new ArrayList<DestinationModel>();
+
+        Cursor cur = db.query(TABLE_NAME_DESTINATIONS,new String[]{DEST_ID,DEST_ID_COMPANY,DEST_CAPACITY,DEST_NAME},null,null,null,null,null);
+        for (int cc=0; cc<cur.getCount();cc++){
+            cur.moveToPosition(cc);
+            data.add(new DestinationModel(Integer.parseInt(cur.getString(0)),Integer.parseInt(cur.getString(1)),Integer.parseInt(cur.getString(2)),cur.getString(3)));
+        }
         db.close();
-        return id;
+        return data;
+    }
+
+//    public String insertCost(CostModel data){
+//        ContentValues contentValues = new ContentValues();
+//        contentValues.put(COST_ID_SOURCE, data.getId_source());
+//        contentValues.put(COST_ID_DEST, data.getId_destination());
+//        contentValues.put(COST_ID_COMPANY, data.getId_company());
+//        contentValues.put(COST_COST, data.getCost());
+//
+//        SQLiteDatabase db = this.getWritableDatabase();
+//        db.insert(TABLE_NAME_COSTS, null, contentValues);
+//        Cursor cur = db.query(TABLE_NAME_COSTS, new String[] {COST_ID},COMPANY_NAME+"="+data.getName(),null,null,null,null);
+//        cur.moveToPosition(0);
+//        String id  = cur.getString(0);
+//        db.close();
+//        return id;
+//    }
+
+    //Method membaca satu cost
+    public CostModel readCost(int ID) {
+        SQLiteDatabase db = this.getReadableDatabase();
+
+        Cursor cursor = db.query(TABLE_NAME_COSTS, new String[] {
+                        COST_ID,
+                        COST_ID_SOURCE,
+                        COST_ID_DEST,
+                        COST_ID_COMPANY,
+                        COST_COST},
+                COST_ID + "=?", new String[] {Integer.toString(ID)}, null, null, null);
+
+        if(cursor != null)
+            cursor.moveToFirst();
+
+        CostModel  data = new CostModel(Integer.parseInt(cursor.getString(0)), Integer.parseInt(cursor.getString(1)), Integer.parseInt(cursor.getString(2)), Integer.parseInt(cursor.getString(3)), Integer.parseInt(cursor.getString(4)));
+        return data;
+    }
+
+    //Membaca semua data costs
+    public ArrayList<CostModel> readCostsAll(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        ArrayList<CostModel> data = new ArrayList<CostModel>();
+
+        Cursor cur = db.query(TABLE_NAME_COSTS,new String[]{COST_ID,COST_ID_SOURCE,COST_ID_DEST,COST_ID_COMPANY,COST_COST},null,null,null,null,null);
+        for (int cc=0; cc<cur.getCount();cc++){
+            cur.moveToPosition(cc);
+            data.add(new CostModel(Integer.parseInt(cur.getString(0)),Integer.parseInt(cur.getString(1)),Integer.parseInt(cur.getString(2)),Integer.parseInt(cur.getString(3)), Integer.parseInt(cur.getString(4))));
+        }
+        db.close();
+        return data;
     }
 }
