ff76d1cc618c1a54a6278b3ad6fa01bdd689a0de
sherlyfekay <sherlyfekay@gmail.com>
2017-05-31 04:57:11 +0700

    Toolbar dan Menu

--- app/src/main/java/namex_project/pens/vammethod/Activity/Company/CompanyActivity.java
+++ app/src/main/java/namex_project/pens/vammethod/Activity/Company/CompanyActivity.java
@@ -2,7 +2,6 @@ package namex_project.pens.vammethod.Activity.Company;
 
 import android.app.Activity;
 import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.Intent;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -13,14 +12,14 @@ import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.GridView;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -30,11 +29,13 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
+import namex_project.pens.vammethod.Activity.About.AboutActivity;
 import namex_project.pens.vammethod.Database.DatabaseHandler;
 import namex_project.pens.vammethod.Database.Model.CompanyModel;
 import namex_project.pens.vammethod.R;
 
-public class CompanyActivity extends Activity {
+public class CompanyActivity extends AppCompatActivity {
+
     RecyclerView list_company;
     FloatingActionButton add_company;
     CompanyGridAdapter company_adapter;
@@ -50,10 +51,13 @@ public class CompanyActivity extends Activity {
 
     ArrayList<CompanyModel> data_company;
 
-@Override
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_company);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         list_company = (RecyclerView) findViewById(R.id.list_company);
         add_company = (FloatingActionButton)findViewById(R.id.add_company);
@@ -147,4 +151,30 @@ public class CompanyActivity extends Activity {
             upload_photo.setVisibility(View.VISIBLE);
         }
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_company, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.about) {
+            Intent intent = new Intent(CompanyActivity.this, AboutActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
